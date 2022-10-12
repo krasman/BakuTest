64316c9560b0f809f2e5b1872bc58cfbfafc234a
User <User@DESKTOP-DRJMU61>
2022-10-12 13:10:06 +0400

    Add project files.

---
+++ ConsoleApp3/ArrayList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ConsoleApp3
+{
+    internal class ArrayList
+    {
+        public int length { get; private set; }
+        private int capacity;
+        public int[] array { get; private set; }
+
+        public void AddValue(int value)
+        {
+            if (length + 1 > capacity)
+            {
+                int[] newArray = new int[Convert.ToInt32(capacity * 1.5)];
+                for (int i = 0; i < length; i++) newArray[i] = array[i];
+                array = newArray;
+            }
+            array[length] = value;
+            length++;
+        }
+
+        public void Output()
+        {
+            for (int i = 0; i < length; i++) Console.Write(array[i] + " ");
+            Console.WriteLine();
+        }
+
+        public void RemoveElementAt(int index)
+        {
+            for (int i = index; i < length - 1; i++)
+                array[i] = array[i + 1];
+            array[length-1] = 0;
+            length--;
+        }
+
+        public int FindFirstElement(int value)
+        {
+            for (int i = 0; i < length; i++)
+                if (array[i] == value) return i;
+            return -1;
+        }
+
+        public int RemoveElement(int value)
+        {
+            int index = FindFirstElement(value);
+            if (index != -1)
+            {
+                this.RemoveElementAt(index);
+                return 0;
+            }
+            else return -1;
+        }
+        public ArrayList(int length)
+        {
+            this.capacity = length;
+            this.array = new int[length];
+            this.length = 0;
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+
+       
+        static int Main(string[] args)
+        {
+            /*foreach (var i in args) Console.WriteLine(i);
+            NotMain();*/
+            ArrayList l = new ArrayList(5);
+            l.AddValue(1);
+            l.AddValue(2);
+            l.AddValue(3);
+            l.AddValue(4);
+            l.AddValue(5);
+            l.AddValue(6);
+            l.Output();
+            Console.WriteLine(l.FindFirstElement(3));
+            l.RemoveElementAt(2);
+            l.Output();
+            l.RemoveElement(5);
+            l.Output();
+            /* int a = Convert.ToInt32(Console.ReadLine());
+             int b = Convert.ToInt32(Console.ReadLine());
+             try
+             {
+                 Console.WriteLine(DivideTwoNumbers(a,b));
+             }
+             catch (DivideByZeroException)
+             {
+                 Console.WriteLine("Srsly?");
+             }*/
+             Console.ReadLine();
+            return 0;
+        }
+
+        static double DivideTwoNumbers(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a * 1.0 / b;
+        }
+
+    }
+}
